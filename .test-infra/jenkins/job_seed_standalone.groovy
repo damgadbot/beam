f6b90ad5e5ec0249931b989f3f31d3d67b837474
Damian Gadomski <damian.gadomski@polidea.com>
2020-06-03 13:44:46 +0200

    changeing repo to damgadbot/beam

--- .test-infra/jenkins/job_seed_standalone.groovy
+++ .test-infra/jenkins/job_seed_standalone.groovy
@@ -25,7 +25,7 @@ job('beam_SeedJob_Standalone') {
       ' on Jenkins DSL groovy files checked into the code repository.')
 
   properties {
-    githubProjectUrl('https://github.com/apache/beam/')
+    githubProjectUrl('https://github.com/damgadbot/beam/')
   }
 
   // Restrict to only run on Jenkins executors labeled 'beam'
@@ -38,7 +38,7 @@ job('beam_SeedJob_Standalone') {
   scm {
     git {
       remote {
-        github('apache/beam')
+        github('damgadbot/beam')
 
         // ${ghprbPullId} is not interpolated by groovy, but passed through to Jenkins where it
         // refers to the environment variable
